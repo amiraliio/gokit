f36e1e236631c1e7df64eb89cc54e52beb83bbb3
amiralii <mr.roshanae@gmail.com>
2020-03-20 00:47:08 +0330

    updated

--- todo/transport.go
+++ todo/transport.go
@@ -15,7 +15,13 @@ func NewTransport(_ context.Context, endpoints *Endpoints) {
 		endpoints.Insert,
 		insertRequest,
 		insertResponse,
-    ))
+	))
+
+	http.Handle("/Todo", httptransport.NewServer(
+		endpoints.List,
+		listRequest,
+		listResponse,
+	))
 
 	log.Fatal(http.ListenAndServe(":8976", nil))
 
@@ -30,5 +36,17 @@ func insertRequest(_ context.Context, request *http.Request) (interface{}, error
 }
 
 func insertResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
+	return json.NewEncoder(w).Encode(response)
+}
+
+func listRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	var listRequest ListTodoRequest
+	if err := json.NewDecoder(r.Body).Decode(&listRequest); err != nil {
+		return nil, err
+	}
+	return listRequest, nil
+}
+
+func listResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
 	return json.NewEncoder(w).Encode(response)
 }
